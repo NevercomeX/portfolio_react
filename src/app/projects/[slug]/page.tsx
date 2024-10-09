beee75385f870ea0294c3ebc49d5bb1ed1107c22
nevercomex <luisjavier2012@gmail.com>
2024-10-09 17:50:43 -0400

    fix

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { promises as fs } from 'fs';
+import fs from 'fs';
 import path from 'path';
 import { compileMDX } from 'next-mdx-remote/rsc';
 import BlockQuote from '@/containers/portfolio-page/mdx/blockquote';
@@ -7,51 +7,33 @@ import H2 from '@/containers/portfolio-page/mdx/H2';
 import HeroImage from '@/containers/portfolio-page/mdx/HeroImage';
 import P from '@/containers/portfolio-page/mdx/P';
 
-// Usar la carpeta public/_posts para asegurar que los archivos estén disponibles
 const POSTS_FOLDER = path.join(process.cwd(), 'public', '_posts');
 
-async function getPostFileNames() {
-  const filenames = await fs.readdir(POSTS_FOLDER);
-  return filenames.map((name) => name.replace(/\.mdx?$/, ''));
-}
-
-async function readPostFile(slug: string) {
-  const filePath = path.join(POSTS_FOLDER, `${slug}.mdx`);
-  try {
-    const fileContent = await fs.readFile(filePath, 'utf8');
-    return fileContent;
-  } catch (error) {
-    console.error(`Error reading the file for slug: ${slug}`, error);
-    return null; // Manejo de errores si no encuentra el archivo
-  }
-}
-
-// Generar los parámetros estáticos de los slugs de los posts
+// Genera los slugs de los posts en tiempo de compilación
 export async function generateStaticParams() {
-  const slugs = await getPostFileNames();
-  return slugs.map((slug) => ({ slug }));
+  const filenames = fs.readdirSync(POSTS_FOLDER);
+  return filenames.map((filename) => ({
+    slug: filename.replace(/\.mdx$/, ''),
+  }));
 }
 
+// Renderiza cada página del post de forma estática
 export default async function PostPage({
   params,
 }: {
   params: { slug: string };
 }) {
-  const markdown = await readPostFile(params.slug);
+  const { slug } = params;
+  const filePath = path.join(POSTS_FOLDER, `${slug}.mdx`);
 
-  if (!markdown) {
-    return { notFound: true }; // Si no encuentra el archivo, devolver 404
+  if (!fs.existsSync(filePath)) {
+    return { notFound: true };
   }
 
-  const { content } = await compileMDX<{ title: string }>({
-    source: markdown,
-    components: {
-      h1: H1,
-      h2: H2,
-      p: P,
-      HeroImage,
-      blockquote: BlockQuote,
-    },
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const { content } = await compileMDX({
+    source: fileContent,
+    components: { h1: H1, h2: H2, p: P, HeroImage, blockquote: BlockQuote },
     options: { parseFrontmatter: true },
   });
 
